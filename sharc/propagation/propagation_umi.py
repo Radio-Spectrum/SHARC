5fdf500412e77bf78a296520734b3a9d5e2eda5d
Bruno Faria <brunohcfaria@gmail.com>
2024-08-05 16:55:41 -0300

    Refactoring of the PropagationTvro model for the new get_loss interface

--- sharc/propagation/propagation_umi.py
+++ sharc/propagation/propagation_umi.py
@@ -61,7 +61,7 @@ class PropagationUMi(Propagation):
             bs_to_ue_dist_2d = station_b.get_distance_to(station_a)
             bs_to_ue_dist_3d = station_b.get_3d_distance_to(station_a)
 
-        loss = self.__get_loss(distance_3D=bs_to_ue_dist_3d,
+        loss = self._get_loss(distance_3D=bs_to_ue_dist_3d,
                                distance_2D=bs_to_ue_dist_2d,
                                frequency=frequency*np.ones(bs_to_ue_dist_2d.shape),
                                bs_height=station_b.height,
@@ -71,7 +71,7 @@ class PropagationUMi(Propagation):
         # the interface expects station_a.num_stations x station_b.num_stations array
         return np.transpose(loss)
     
-    def __get_loss(self, *args, **kwargs) -> np.array:
+    def _get_loss(self, *args, **kwargs) -> np.array:
         """
         Calculates path loss for LOS and NLOS cases with respective shadowing
         (if shadowing is to be added)

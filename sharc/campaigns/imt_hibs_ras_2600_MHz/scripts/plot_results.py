afbad524c4aa195a2792428cf099a3db2f905c7a
artistrea <apiravesi@gmail.com>
2024-08-19 15:30:59 -0300

    Revert "update(campaigns): plot_results"
    This reverts commit 801891735e05524c4a690d23f47e523d808f13cb.

--- sharc/campaigns/imt_hibs_ras_2600_MHz/scripts/plot_results.py
+++ sharc/campaigns/imt_hibs_ras_2600_MHz/scripts/plot_results.py
@@ -1,32 +1,30 @@
-import os
 from sharc.plots.plot_cdf import all_plots
 
 # Define the base directory
-workfolder = os.path.dirname(os.path.abspath(__file__))
-campaign_base_folder = workfolder.split("/")[-2]
+name = "imt_hibs_ras_2600_MHz"
 
-# campaign_base_folder = "imt_hibs_ras_2600_MHz"
+# Example with specific subfolders and legends
+# Define legend names for the different subdirectories
+legends = ["0 Km", "45 Km", "90 Km", "500 Km"]
 
-# This will include all subfolders that start with "output_imt_hibs_ras_2600_MHz_" in the f"{base_directory}/output/"
+# Define specific subfolders if needed
+subfolder = [
+    "output_imt_hibs_ras_2600_MHz_0km_2024-07-30_01", 
+    "output_imt_hibs_ras_2600_MHz_45km_2024-07-30_01", 
+    "output_imt_hibs_ras_2600_MHz_90km_2024-07-30_01", 
+    "output_imt_hibs_ras_2600_MHz_500km_2024-07-30_01"
+]
+
+# Run the function with specific subfolders and legends
+all_plots(name, legends=legends, subfolder=subfolder, save_file=True, show_plot=False)
+
+# Example with all subfolders and no specific legends
+# This will include all subfolders that start with "output_imt_hibs_ras_2600_MHz_" in the base directory
 # and generate legends automatically based on the folder names
 
 # Define legends and subfolders as None to include all automatically
-all_plots(campaign_base_folder, legends=None, subfolders=None, save_file=False, show_plot=True)
+#legends = None
+#subpastas = None
 
-# ALTERNATIVE: specify some folders and legends to plot
-
-# Example with specific subfolders and legends
-
-# Define legend names for the different subdirectories
-# legends = ["0 Km", "45 Km", "90 Km", "500 Km"]
-
-# Define specific subfolders if needed
-# subfolders = [
-#     "output_imt_hibs_ras_2600_MHz_0km_2024-07-30_01", 
-#     "output_imt_hibs_ras_2600_MHz_45km_2024-07-30_01", 
-#     "output_imt_hibs_ras_2600_MHz_90km_2024-07-30_01", 
-#     "output_imt_hibs_ras_2600_MHz_500km_2024-07-30_01"
-# ]
-
-# Run the function with specific subfolders and legends
-# all_plots(campaign_base_folder, legends=legends, subfolders=subfolders, save_file=False, show_plot=True)
+# Run the function with all subfolders and auto-generated legends
+#all_plots(name, legends=legends, subpastas=subpastas, save_file=True, show_plot=False)

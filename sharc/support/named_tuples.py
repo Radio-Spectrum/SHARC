5bee69d4f7d70d596579fb57c2ceaf723402639e
artistrea <apiravesi@gmail.com>
2024-10-21 15:17:14 -0300

    update: proof of concept of imt nested parameters

--- sharc/support/named_tuples.py
+++ sharc/support/named_tuples.py
@@ -9,7 +9,7 @@ from collections import namedtuple
 
 AntennaPar = namedtuple(
     "AntennaPar",
-    "adjacent_antenna_model normalization normalization_data element_pattern \
+    "normalization normalization_data element_pattern \
                         element_max_g element_phi_3db element_theta_3db element_am element_sla_v n_rows \
                         n_columns element_horiz_spacing element_vert_spacing multiplication_factor \
                         minimum_array_gain downtilt",

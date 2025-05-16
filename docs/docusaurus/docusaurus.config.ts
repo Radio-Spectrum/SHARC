e9a78996a9db83521d540379a77f6c489b32bdb0
Thiago Ferreira <thiagoferreira@MacBook-Air-de-Thiago.local>
2025-05-16 14:11:46 -0300

    Added a social card

--- docs/docusaurus/docusaurus.config.ts
+++ docs/docusaurus/docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/sharc-social-card.png',
     navbar: {
       title: 'Radio-Spectrum SHARC',
       logo: {

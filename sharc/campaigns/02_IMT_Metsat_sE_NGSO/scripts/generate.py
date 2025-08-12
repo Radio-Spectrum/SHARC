45e988e94515ebd91e064e3c967cfebd72d4ac84
Achiles <achiles.mota@ene.unb.com>
2025-08-12 15:18:59 -0300

    Fixing micro scenario

--- sharc/campaigns/02_IMT_Metsat_sE_NGSO/scripts/generate.py
+++ sharc/campaigns/02_IMT_Metsat_sE_NGSO/scripts/generate.py
@@ -30,14 +30,14 @@ for sistema in ["Sat_E&G", "Sat_C&S"]:
     modified_text[103 - 1] = f"    pattern: {'ITU-R S.465' if sistema == "Sat_Q" else 'ITU-R S.465'}\n"
     modified_text[101 - 1] = f"{'    itu_r_s_465:' if sistema == "Sat_Q" else '    itu_r_s_465:'}\n"
 
-    for imt_cell in ["macro","micro"]:
+    for imt_cell in ["macro", "micro"]:
         modified_text[17 - 1] = f"{'        vertical_beamsteering_range: !!python/tuple [90., 100.]' if imt_cell == "macro" else '        vertical_beamsteering_range: !!python/tuple [90., 120.]'}\n"
         modified_text[38 - 1] = f"    height: {'18' if imt_cell == "macro" else '6'}\n"
         modified_text[29 - 1] = f"        n_columns: {'16' if imt_cell == "macro" else '8'}\n"
         modified_text[35 - 1] = f"          is_enabled: {'true' if imt_cell == "macro" else 'false'}\n"
         modified_text[26 - 1] = f"        element_vert_spacing: {'2.1' if imt_cell == "macro" else '0.7'}\n"
         modified_text[37 - 1] = f"    conducted_power: {'22' if imt_cell == "macro" else '16'}\n"
-        modified_text[137 - 1] = f"    Hte: {'16' if imt_cell == "macro" else '8'}\n"
+        modified_text[137 - 1] = f"    Hte: {'18' if imt_cell == "macro" else '6'}\n"
         modified_text[56 - 1] = f"{'    macrocell:' if imt_cell == "macro" else '    hotspot:'}\n"
         modified_text[63 - 1] = f"    type: {'MACROCELL' if imt_cell == "macro" else 'HOTSPOT'}\n"
         modified_text[60 - 1] = f"{' ' if imt_cell == "macro" else '      num_hotspots_per_cell: 3'}\n"
@@ -48,17 +48,20 @@ for sistema in ["Sat_E&G", "Sat_C&S"]:
         modified_text[82 - 1] = f"{' ' if imt_cell == "macro" else '    distribution_azimuth: NORMAL'}\n"
         modified_text[42 - 1] = f"  channel_model: {'UMa' if imt_cell == "macro" else 'UMi'}\n"
         modified_text[18 - 1] = f"        downtilt: {'6' if imt_cell == "macro" else '10'}\n"
+        modified_text[50 - 1] = f"  minimum_separation_distance_bs_ue: {'35' if imt_cell == "macro" else '5'}\n"
+        modified_text[90 - 1] = f"    p_o_pusch: {'-92.2' if imt_cell == "macro" else '-87.2'}\n"
+  
 
         for p_percentage in [20, 'RANDOM', 'RANDOM_CENARIO']: ##[20, 'RANDOM', 'RANDOM_CENARIO']
             modified_text[143 - 1] = f"    percentage_p: {p_percentage}\n"
 
-            for clutter_type in ['one_end']: ##['one_end', 'both_ends']
+            for clutter_type in ['both_ends']: ##['one_end', 'both_ends']
                 modified_text[147 - 1] = f"    clutter_type: {clutter_type}\n"
 
-                for link_type in ['ul','dl']: ## ['ul', 'dl']
+                for link_type in ['dl']: ## ['ul', 'dl']
                     modified_text[4 - 1] = f"  imt_link: {'DOWNLINK' if link_type == "dl" else 'UPLINK'}\n"
 
-                    for distance in [100, 200]: # In km
+                    for distance in [5, 10]: # In km
                         modified_text[124 - 1] = f"        x: {int(distance) * 1000 + 1500}\n"
                         # Modify seed
                         num = random.randint(0, 1000)

f74f3e64f289db704c1cb30bc8e92b1339a83df4
Achiles <achiles.mota@ene.unb.com>
2025-08-11 21:03:33 -0300

    dsa

--- sharc/campaigns/02_IMT_Metsat_sE_NGSO/scripts/start_simulations_multi_thread.py
+++ sharc/campaigns/02_IMT_Metsat_sE_NGSO/scripts/start_simulations_multi_thread.py
@@ -4,7 +4,7 @@ from sharc.run_multiple_campaigns_mut_thread import run_campaign
 # Set the campaign name
 # The name of the campaign to run. This should match the name of the
 # campaign directory.
-name_campaign = "02_IMT_macro_Metsat_sE_GSO"
+name_campaign = "02_IMT_Metsat_sE_NGSO"
 
 # Run the campaigns
 # This function will execute the campaign with the given name.

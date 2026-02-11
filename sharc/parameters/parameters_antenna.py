42b3ea5680f10603dde7e1225bc9d680486e717b
Fabricio Campos <f.farocampos@gmail.com>
2026-02-11 18:57:07 +0000

    update(antenna): changed ParametersAntennaWithDiameter as the parameter class

--- sharc/parameters/parameters_antenna.py
+++ sharc/parameters/parameters_antenna.py
@@ -96,43 +96,8 @@ class ParametersAntenna(ParametersBase):
         default_factory=ParametersAntennaS672,
     )
 
-    @dataclass
-    class ParametersAntennaRF1245(ParametersBase):
-        """
-        Parameters for ITU-R F.1245 antenna model. It's commonly used
-        for fixed service antennas.
-
-        Paremeters
-        ----------
-        gain : float, default=-25
-            Antenna gain in dB.
-        diameter : float, optional
-            Antenna diameter in meters.
-        frequency : float, optional
-            Operating frequency.
-        """
-        gain: float = -25
-        diameter: float = None
-        frequency: float = None
-
-        def validate(self, ctx):
-            """
-            Validate the antenna parameters for correctness.
-
-            Parameters
-            ----------
-            ctx : str
-                Context string for error messages.
-            Raises
-            ------
-            ValueError
-                If any parameter is invalid.
-            """
-            if None in [self.gain, self.diameter, self.frequency]:
-                raise ValueError(f"{ctx}.antenna_3_dB should be set to a number")
-
-    itu_r_f_1245_fs: ParametersAntennaRF1245 = field(
-        default_factory=ParametersAntennaRF1245,
+    itu_r_f_1245_fs: ParametersAntennaWithDiameter = field(
+        default_factory=ParametersAntennaWithDiameter,
     )
 
     def set_external_parameters(self, **kwargs):
